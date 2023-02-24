10494511d160a395c22b7332fb78a900184ef090
Studentabel <abelmorad97@gmail.com>
2023-02-24 17:37:42 +0800

    Update

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 // dark mode & light mode
 const colorMode  = document.body;
-const colorModeBtn = document.querySelector('.fa-solid');
+const headerMode = document.getElementById('header');
+const searchBtn = document.getElementById('searchBtn');
+const searchImg = document.getElementById('searchImg');
+const searchFieldMode = document.getElementById('searchBar');
+const colorModeBtn = document.querySelector('.fa-regular');
 const colorToggle = document.querySelector('.color-toggle');
-const headerColor = document.getElementById('header');
 
 colorModeBtn.addEventListener('click', colorChange);
 
 function colorChange() {
     colorMode.classList.toggle('dark-mode');
-    headerColor.classList.toggle('header-dark');
+    headerMode.classList.toggle('header-dark');
+    searchFieldMode.classList.toggle('search-field-dark');
+    searchBtn.classList.toggle('search-btn-dark');
+    searchImg.classList.toggle('fa-magnifying-glass-dark');
 
+    // Replaces text when toggled
     if (colorMode.classList.contains('dark-mode')) {
         colorToggle.textContent = ' Light Mode';
     } else {
